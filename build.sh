229c5ebc3c291e8ef82490aa9138288c07a23988
eloyjm <eloyjm93@gmail.com>
2023-06-05 17:20:46 +0200

    admin

--- build.sh
+++ build.sh
@@ -4,10 +4,11 @@ set -o errexit
 
 pip install -r requirements.txt
 
+if [[ $CREATE_SUPERUSER ]];
+then
+  python myweather/manage.py createsuperuser --no-input
+fi
+
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-if [[ $CREATE_SUPERUSER ]];
-then
-  python world_champ_2022/manage.py createsuperuser --no-input
-fi
